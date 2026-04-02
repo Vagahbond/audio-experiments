c1fba1f2a598f851f220b3bbb9c84ea43e48e1a7
Vagahbond <vagahbond@pm.me>
2026-04-02 14:46:53 +1300

    strings: len to freq

--- wasm/string_length/src/lib.rs
+++ wasm/string_length/src/lib.rs
@@ -1,11 +1,19 @@
 use wasm_bindgen::prelude::*;
 
+
 #[wasm_bindgen]
-pub fn midi_to_freq(key: u8) -> f64 {
-    core::midi::midi_to_freq(key)
+pub fn string_length2midi(length: f64) -> u8 {
+    core::midi::string_length2midi(length)
 }
 
 #[wasm_bindgen]
-pub fn freq_to_midi(freq: f64) -> u8 {
-    core::midi::freq_to_midi(freq)
+pub fn string_midi2length(midi: u8) -> f64 {
+    core::midi::string_midi2length(midi)
 }
+
+#[wasm_bindgen]
+pub fn string_length2freq(length: f64) -> f64 {
+    core::midi::string_length2freq(length)
+} 
+
+
